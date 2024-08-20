80e047f3f4476e42a19c6f67ae7affce7eca70fa
Bernardo Zaparoli <bernardozaparoli@hotmail.com>
2024-08-19 23:49:13 -0300

    Inicializacao do API Passport

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\HolidayPlanControllerAPI;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [HolidayPlanControllerAPI::class, 'index'])->name('index');
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
 
-Route::resource('holiday', HolidayPlanControllerAPI::class)->names([
-    'index' => 'holiday.index',
-    'create' => 'holiday.add',
-    'store' => 'holiday.save',
-    'show' => 'holiday.show',
-    'edit' => 'holiday.edit',
-    'update' => 'holiday.update',
-    'destroy' => 'holiday.destroy',
-]);
+Route::middleware('auth:api')->group(function () {
+    Route::resource('holiday', HolidayPlanControllerAPI::class)->names([
+        'index' => 'holiday.index',
+        'create' => 'holiday.add',
+        'store' => 'holiday.save',
+        'show' => 'holiday.show',
+        'edit' => 'holiday.edit',
+        'update' => 'holiday.update',
+        'destroy' => 'holiday.destroy',
+    ]);
+});
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PDFController;
 
-Route::get('/', function () {
-    return redirect('/api');
-});
-
 Route::get('/holiday/generate-pdf/{id}', [PDFController::class, 'generatePDF'])->name('holiday.generatePDF');
